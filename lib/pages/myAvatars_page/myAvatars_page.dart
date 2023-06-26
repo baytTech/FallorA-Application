ef693d846d6a125476ec8bd3406027bb69012d83
Betül Kırçıl <92184238+BetullKircil@users.noreply.github.com>
2023-06-26 12:01:53 +0300

    [DEV][Betul.Kircil][Added appBar]

--- lib/pages/myAvatars_page/myAvatars_page.dart
+++ lib/pages/myAvatars_page/myAvatars_page.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+
+import '../../widgets/appbar/FalloraAppBar.dart';
 
 class MyAvatarsPage extends StatefulWidget {
   const MyAvatarsPage({super.key});
@@ -11,6 +14,30 @@ class MyAvatarsPage extends StatefulWidget {
 class _MyAvatarsPageState extends State<MyAvatarsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: FalloraAppBar(
+        isHome: true,
+        context: context,
+        gradient: LinearGradient(
+          colors: [Color(0xFF31113B), Color(0xFF258195)],
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    title: Text('Öğe $index'),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
