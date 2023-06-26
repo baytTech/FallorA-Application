692e87a5afa5298d2507bd53d89757ac27e1d60b
Betül Kırçıl <92184238+BetullKircil@users.noreply.github.com>
2023-06-26 11:01:04 +0300

    [DEV][Betul.Kircil][created avatarlarim page]

---
+++ lib/pages/myAvatars_page/myAvatars_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class MyAvatarsPage extends StatefulWidget {
+  const MyAvatarsPage({super.key});
+
+  @override
+  State<MyAvatarsPage> createState() => _MyAvatarsPageState();
+}
+
+class _MyAvatarsPageState extends State<MyAvatarsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
